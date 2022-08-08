c046eaad0ae45c940a3dcc20ba34a555b0a7c097
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-08-08 21:45:35 +0200

    Initial Commit

---
+++ Webapp/React Dashboard/ava-dash/src/pages/index.jsx
@@ -0,0 +1,5 @@
+export { default as Home } from './Home';
+export { default as Weather } from './Weather';
+//CHarts
+export { default as Line } from './Charts/Line';
+export { default as Area } from './Charts/Area';
